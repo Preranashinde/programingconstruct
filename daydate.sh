0b12efcadc9b9738215ad2a8b2cf1aa0d5c3ec99
Preranashinde <preranapshinde3009@gmail.com>
2020-03-12 17:59:50 +0530

    ADDED:The program to print day of month is added

---
+++ daydate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+read day 
+read month
+if (( $day < 1 || $day > 31 || $month < 3 || $month >6 ))
+	then
+		echo "invalid entry"
+else
+	if (( $month <= 6 && $month >= 3 ))
+	then
+			if (( $month == 3 && $day >= 20 ))
+		then
+			echo "true"
+		elif (( $month == 6 && $day <= 20 ))
+		then
+			echo "true"
+		elif (( $month == 4 && $day <=30 || $month == 5 ))
+		then
+			echo "true"
+		else
+			echo "false"
+			fi
+		else
+			echo "false"
+	fi	
+fi
