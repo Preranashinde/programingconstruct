1ea36a5dc7a72d554261c02a87602cea9683628a
Preranashinde <preranapshinde3009@gmail.com>
2020-03-16 15:12:52 +0530

    ADDED:Read single digit number and print in word

---
+++ case1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+read -p "enter single digit number" number 
+echo $number
+case $number in
+
+	0)
+		echo "zero" ;;
+	1)
+		echo "one" ;;
+	2)
+      echo "Two" ;;
+	3)
+      echo "Three" ;;
+	4)
+      echo "four" ;;
+	5)
+      echo "five" ;;
+	6)
+      echo "six" ;;
+	7)
+      echo "seven" ;;
+	8)
+      echo "eight" ;;
+	9)
+      echo "nine" ;;
+esac  
+
